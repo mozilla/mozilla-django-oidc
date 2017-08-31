93ee0f49aacc683c9eae46a747f587191a9c5628
Dustin J. Mitchell <dustin@mozilla.com>
2017-08-31 15:54:47 +0000

    Document and implement OIDC_STORE_{ACCESS,ID}_TOKEN

--- mozilla_django_oidc/auth.py
+++ mozilla_django_oidc/auth.py
@@ -135,6 +135,9 @@ class OIDCAuthenticationBackend(object):
             access_token = token_response.get('access_token')
 
             if import_from_settings('OIDC_STORE_ACCESS_TOKEN', False):
+                session['oidc_access_token'] = access_token
+
+            if import_from_settings('OIDC_STORE_ID_TOKEN', False):
                 session['oidc_id_token'] = id_token
 
             user_response = requests.get(self.OIDC_OP_USER_ENDPOINT,
