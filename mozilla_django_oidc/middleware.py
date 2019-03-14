1244996544a4bb0433a482a1da2560f9f5057ca9
Germano Guerrini <germano.guerrini@acsys.it>
2019-03-14 11:16:28 +0100

    Make lint happy. Removed duplicated tests.

--- mozilla_django_oidc/middleware.py
+++ mozilla_django_oidc/middleware.py
@@ -84,7 +84,6 @@ class SessionRefresh(MiddlewareMixin):
             request.path not in self.exempt_urls
         )
 
-
     def is_expired(self, request):
         if not self.is_refreshable_url(request):
             LOGGER.debug('request is not refreshable')
