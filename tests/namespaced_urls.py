a5990a0f934fa556c2381ceb0474523864007f6f
Jaap Roes <jroes@leukeleu.nl>
2019-10-14 11:20:44 +0200

    Remove backcompat workarounds for unsupported Django versions from tests

--- tests/namespaced_urls.py
+++ tests/namespaced_urls.py
@@ -1,8 +1,3 @@
-import django
 from django.conf.urls import url, include
 
-
-if django.VERSION < (1, 8, 99):
-    urlpatterns = [url(r'^namespace/', include('mozilla_django_oidc.urls', namespace='namespace'))]
-else:
-    urlpatterns = [url(r'^namespace/', include(('mozilla_django_oidc.urls', 'namespace')))]
+urlpatterns = [url(r'^namespace/', include(('mozilla_django_oidc.urls', 'namespace')))]
