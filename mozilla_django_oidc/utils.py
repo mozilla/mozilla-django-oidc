89896110e556e1a1be92a361d69c7cbf02717b45
Florian Hartmann <hartmann.florian@siemens.com>
2020-07-12 18:53:57 +0200

    Added docstring comment to the `add_state_and_nonce_to_session` function.

--- mozilla_django_oidc/utils.py
+++ mozilla_django_oidc/utils.py
@@ -60,6 +60,12 @@ def is_authenticated(user):
 
 
 def add_state_and_nonce_to_session(request, state, params):
+    """
+    Stores the `state` and `nonce` parameters in a session dictionary including the time when it was added.
+    The dictionary can contain multiple state/nonce combinations to allow parallel logins with multiple browser
+    sessions.
+    To keep the session space to a reasonable size, the dictionary is kept at 50 state/nonce combinations maximum.
+    """
     nonce = None
     if import_from_settings('OIDC_USE_NONCE', True):
         nonce = get_random_string(import_from_settings('OIDC_NONCE_SIZE', 32))
