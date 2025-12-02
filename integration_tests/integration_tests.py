7c722e4de02c06f100b0df01fe5dcf66b3979885
Ryan Johnson <rjohnson@mozilla.com>
2025-12-02 13:50:47 -0800

    replace CircleCI with GHA for integration tests

--- integration_tests/integration_tests.py
+++ integration_tests/integration_tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from selenium.webdriver.firefox.service import Service
 from splinter import Browser, Config
 
 
@@ -13,11 +14,14 @@ class IntegrationTest(unittest.TestCase):
             "password": "example_p@ssw0rd",
             "email": "example@example.com",
         }
-        self.splinter_config = Config(headless=True)
+        self.config = Config(headless=True)
+        self.service = Service(executable_path="/usr/bin/geckodriver")
 
     def setUp(self):
         """Create test account in `testprovider` instance"""
-        with Browser(self.webdriver, config=self.splinter_config) as browser:
+        with Browser(
+            self.webdriver, config=self.config, service=self.service
+        ) as browser:
             browser.visit("http://testprovider:8080/account/signup")
             browser.find_by_css("#id_username").fill(self.account["username"])
             browser.find_by_css("#id_password").fill(self.account["password"])
@@ -27,7 +31,9 @@ class IntegrationTest(unittest.TestCase):
 
     def tearDown(self):
         """Remove test account from `testprovider` instance"""
-        with Browser(self.webdriver, config=self.splinter_config) as browser:
+        with Browser(
+            self.webdriver, config=self.config, service=self.service
+        ) as browser:
             self.perform_login(browser)
             browser.visit("http://testprovider:8080/account/delete")
             browser.find_by_css(".btn-danger").click()
@@ -47,41 +53,43 @@ class IntegrationTest(unittest.TestCase):
 
     def test_login(self):
         """Test logging in `testrp` using OIDC"""
-        browser = Browser(self.webdriver, config=self.splinter_config)
+        with Browser(
+            self.webdriver, config=self.config, service=self.service
+        ) as browser:
+            # Check that user is not logged in
+            browser.visit("http://testrp:8081")
+            self.assertTrue(browser.is_text_not_present("Current user:"))
 
-        # Check that user is not logged in
-        browser.visit("http://testrp:8081")
-        self.assertTrue(browser.is_text_not_present("Current user:"))
+            # Perform login
+            self.perform_login(browser)
 
-        # Perform login
-        self.perform_login(browser)
+            # Accept scope
+            browser.find_by_css('input[name="allow"]').click()
 
-        # Accept scope
-        browser.find_by_css('input[name="allow"]').click()
-
-        # Check that user is now logged in
-        self.assertTrue(browser.is_text_present("Current user:"))
+            # Check that user is now logged in
+            self.assertTrue(browser.is_text_present("Current user:"))
 
     def test_logout(self):
         """Test logout functionality of OIDC lib"""
-        browser = Browser(self.webdriver, config=self.splinter_config)
+        with Browser(
+            self.webdriver, config=self.config, service=self.service
+        ) as browser:
+            # Check that user is not logged in
+            browser.visit("http://testrp:8081")
+            self.assertTrue(browser.is_text_not_present("Current user:"))
 
-        # Check that user is not logged in
-        browser.visit("http://testrp:8081")
-        self.assertTrue(browser.is_text_not_present("Current user:"))
+            self.perform_login(browser)
 
-        self.perform_login(browser)
+            # Accept scope
+            browser.find_by_css('input[name="allow"]').click()
 
-        # Accept scope
-        browser.find_by_css('input[name="allow"]').click()
+            # Check that user is now logged in
+            self.assertTrue(browser.is_text_present("Current user:"))
 
-        # Check that user is now logged in
-        self.assertTrue(browser.is_text_present("Current user:"))
+            self.perform_logout(browser)
 
-        self.perform_logout(browser)
-
-        # Check that user is now logged out
-        self.assertTrue(browser.is_text_not_present("Current user:"))
+            # Check that user is now logged out
+            self.assertTrue(browser.is_text_not_present("Current user:"))
 
 
 if __name__ == "__main__":
